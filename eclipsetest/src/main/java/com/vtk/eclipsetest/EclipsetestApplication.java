9bd5512ac94fb4df597f62cf660f51be77e95773
thanhkieuus <voltakieu@yahoo.com>
2018-05-26 22:02:57 -0500

    First Commit

---
+++ eclipsetest/src/main/java/com/vtk/eclipsetest/EclipsetestApplication.java
@@ -0,0 +1,12 @@
+package com.vtk.eclipsetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EclipsetestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EclipsetestApplication.class, args);
+	}
+}
